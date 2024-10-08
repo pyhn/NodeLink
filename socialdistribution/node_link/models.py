c282b46f01413138b8673bac8bfde1dd1090576e
frahma1 <frahma1@ualberta.ca>
2024-10-08 13:13:09 -0600

    Created user  models

--- socialdistribution/node_link/models.py
+++ socialdistribution/node_link/models.py
@@ -1,16 +1,24 @@
 from django.db import models
 from datetime import datetime
+from django.contrib.auth.models import AbstractUser
 
 
-class Admin(models.Model):
+class User(AbstractUser):
     first_name = models.CharField(max_length=20, null=False)
     last_name = models.CharField(max_length=20, null=False)
     date_ob = models.DateField(null=False)
     profile_img = models.FilePathField(null=True)
-    screen_name = models.CharField(max_length=20, null=False)
+    username = models.CharField(max_length=20, null=False)
     description = models.TextField(null=False)
     join_date = models.DateField(null=False, default=datetime.now())
-    email = models.CharField(max_length=20, null=False)
+    email = models.EmailField(max_length=20, null=False, unique=True)
+
+
+class Admin(User):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username} (Node Admin)"
 
 
 class Node(models.Model):
@@ -23,19 +31,15 @@ class Node(models.Model):
     deleted_by = models.ForeignKey(Admin)
 
 
-class Author(models.Model):
-    first_name = models.CharField(max_length=20, null=False)
-    last_name = models.CharField(max_length=20, null=False)
-    date_ob = models.DateField(null=False)
-    profile_img = models.FilePathField(null=True)
-    screen_name = models.CharField(max_length=20, null=False)
-    description = models.TextField(null=False)
-    join_date = models.DateField(null=False, default=datetime.now())
-    email = models.CharField(max_length=20, null=False)
+class Author(User):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     github_url = models.CharField(null=True)
     github_token = models.CharField(null=True)
     github_user = models.CharField(null=True)
     local_node = models.ForeignKey(Node, null=False)
+
+    def __str__(self):
+        return f"{self.user.username} (Author)"
 
 
 class MixinApp(models.Model):
