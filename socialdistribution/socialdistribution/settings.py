1ab4cd831a652cf2edfc803d34bc56d4973d222e
AivanD <dolor@ualberta.ca>
2024-10-08 21:21:05 -0600

    - removed the other requirements.txt that is not in the root (need it to be in the root based on the instruction for setting up heroku) - updated root requirements.txt - created procfile - modded settings.py based on heroku instruction (lab3)

--- socialdistribution/socialdistribution/settings.py
+++ socialdistribution/socialdistribution/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-x8c5b#@##1*0%%@vfry^8e)gdc-lxwd8&sjt#!%iwxm=6jzv5w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = "node_link.User"
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware"
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -117,7 +118,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "static/"
 
 # Default primary key field type
