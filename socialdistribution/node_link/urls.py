985e7f21dc65dbfe0a165c18451332432e0971d3
frahma1 <frahma1@ualberta.ca>
2024-10-15 16:41:20 -0600

    made skelton for home page

--- socialdistribution/node_link/urls.py
+++ socialdistribution/node_link/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = "node_link"
 urlpatterns = [
-    path('signup/', views.signup_view, name='signup'),
-    path('login/', views.login_view, name='login'),
-    path('', views.home_view, name='home'),
-    path('logout/', views.logout_view, name='logout'),
+    path("signup/", views.signup_view, name="signup"),
+    path("login/", views.login_view, name="login"),
+    path("", views.Home.as_view(), name="home"),
+    path("logout/", views.logout_view, name="logout"),
 ]
