695c6011d617edd7b60ed7d00859e0486a468bb0
pyhn <reimarkronabio@gmail.com>
2024-10-15 18:31:16 -0600

    fixed settings to be cleaner

--- socialdistribution/socialdistribution/settings.py
+++ socialdistribution/socialdistribution/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os  #, dj_database_url
+import os, dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 ]
-AUTH_USER_MODEL = "node_link.User" 
+AUTH_USER_MODEL = "node_link.User"
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
@@ -85,14 +85,13 @@ WSGI_APPLICATION = "socialdistribution.wsgi.application"
 
 if os.environ.get("DATABASE_URL") != None:
     # Running on Heroku
-    pass
-    # DATABASES = {
-    #     "default": dj_database_url.config(
-    #         conn_max_age=600,
-    #         conn_health_checks=True,
-    #         ssl_require=True
-    #     )
-    # }
+    DATABASES = {
+        "default": dj_database_url.config(
+            conn_max_age=600,
+            conn_health_checks=True,
+            ssl_require=True
+        )
+    }
 else:
     # Running locally.
     DATABASES = {
