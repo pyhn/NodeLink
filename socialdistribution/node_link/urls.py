820114e4a608284e206fa280d4734283495c4da0
pyhn <reimarkronabio@gmail.com>
2024-10-16 20:16:56 -0600

    removed dupliate home paths

--- socialdistribution/node_link/urls.py
+++ socialdistribution/node_link/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
     path("create_post/", views.create_post, name="create_post"),
     path("posts_list/", views.post_list, name="post_list"),
     path("posts_list/<int:id>/", views.post_detail, name="post_detail"),
