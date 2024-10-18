fccae2e05471e07cb1e6ffa24bd238c1b508c212
pyhn <reimarkronabio@gmail.com>
2024-10-18 14:15:38 -0600

    fixed html to include the right information from the author

--- socialdistribution/node_link/urls.py
+++ socialdistribution/node_link/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path("create_post/", views.create_post, name="create_post"),
     path("posts_list/", views.post_list, name="post_list"),
-    path("posts_list/<int:id>/", views.post_detail, name="post_detail"),
+    path("posts_list/<int:post_id>/", views.post_detail, name="post_detail"),
     path("create_comment/<int:post_id>/", views.create_comment, name="create_comment"),
     path("like_post/<int:post_id>/", views.like_post, name="like_post"),
     path("signup/", views.signup_view, name="signup"),
