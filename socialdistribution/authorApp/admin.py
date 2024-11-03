a66dec6172fa0ca851439b8b75217b3e2e70e764
frahma1 <frahma1@ualberta.ca>
2024-11-03 05:51:27 -0700

    changed on_delete to allow user deletion

--- socialdistribution/authorApp/admin.py
+++ socialdistribution/authorApp/admin.py
@@ -4,7 +4,22 @@ from django.contrib import admin
 # Project Imports
 from .models import User, AuthorProfile, Friends, Follower
 
-admin.site.register(User)
 admin.site.register(AuthorProfile)
 admin.site.register(Friends)
 admin.site.register(Follower)
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["username", "email", "is_approved"]
+    actions = ["approve_users"]
+    list_filter = [
+        "is_approved",
+    ]  # Add the custom filter
+
+    def approve_users(self, request, queryset):
+        queryset.update(is_approved=True)
+
+    approve_users.short_description = "Approve selected users"
+
+
+admin.site.register(User, UserAdmin)
