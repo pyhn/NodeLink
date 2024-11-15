3736b9318404353a89595e9237b708ad92adda83
martinmj4im <martinmj4im@gmail.com>
2024-11-15 11:17:31 -0700

    Configured PostgreSQL database for Heroku deployment

--- socialdistribution/socialdistribution/settings.py
+++ socialdistribution/socialdistribution/settings.py
@@ -89,21 +89,18 @@ WSGI_APPLICATION = "socialdistribution.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-if os.environ.get("DATABASE_URL") is not None:
-    DATABASES = {
-        "default": dj_database_url.config(
-            conn_max_age=600, conn_health_checks=True, ssl_require=True
-        )
+# Default to SQLite
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
 
-else:
-    # Running locally.
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+# Configure from DATABASE_URL environment variable if it exists
+DATABASE_URL = os.environ.get('DATABASE_URL')
+if DATABASE_URL:
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
