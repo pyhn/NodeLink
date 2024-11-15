fe76284f6202f590f60f5215d072a39d803cd98d
pyhn <reimarkronabio@gmail.com>
2024-11-14 20:32:16 -0700

    removed uncessary api viewsets and removed follow request api

--- socialdistribution/authorApp/urls.py
+++ socialdistribution/authorApp/urls.py
@@ -29,9 +29,4 @@ urlpatterns = [
     ),
     path("unfriend/<int:friend_id>/", views.unfriend, name="unfriend"),
     path("api/", include(router.urls)),
-    path(
-        "api/authors/<str:author_serial>/inbox",
-        views.FollowRequestView.as_view(),
-        name="follow-request",
-    ),
 ]
