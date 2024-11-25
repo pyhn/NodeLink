89958bc1ac6866902618cbf74c83377280c660a5
pyhn <reimarkronabio@gmail.com>
2024-11-24 19:25:40 -0700

    increasing lengths

--- socialdistribution/authorApp/models.py
+++ socialdistribution/authorApp/models.py
@@ -11,7 +11,8 @@ class User(AbstractUser):
     date_ob = models.DateField(null=True, blank=True)
     is_approved = models.BooleanField(default=False)  # Track approval status
     profileImage = models.URLField(
-        default="https://s3.amazonaws.com/37assets/svn/765-default-avatar.png"
+        default="https://s3.amazonaws.com/37assets/svn/765-default-avatar.png",
+        max_length=255,
     )
     display_name = models.CharField(max_length=150, null=False, blank=False)
     github_user = models.CharField(max_length=255, null=True, blank=True)
@@ -41,7 +42,7 @@ class AuthorProfile(models.Model):
     github = models.CharField(max_length=255, null=True, blank=True)
     github_token = models.CharField(max_length=255, null=True, blank=True)
     github_user = models.CharField(max_length=255, null=True, blank=True)
-    last_github_event_id = models.CharField(max_length=50, null=True, blank=True)
+    last_github_event_id = models.CharField(max_length=150, null=True, blank=True)
     fqid = models.TextField(
         blank=True, editable=False, unique=True
     )  # New field for fqid
